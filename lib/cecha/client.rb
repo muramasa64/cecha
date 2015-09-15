7bb622b498451596ec256acc1ec6f66ab187e3f9
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-09-15 18:04:07 +0900

    output aws_account_number and region

--- lib/cecha/client.rb
+++ lib/cecha/client.rb
@@ -5,7 +5,15 @@ module Cecha
   class Client
     attr_reader :logger
 
-    ElbCert = Struct.new("ElbCert", :elb_name, :elb_port, :ssl_certificate_name, :signature_algorithm) do
+    ElbCert = Struct.new(
+      "ElbCert",
+      :aws_account_number,
+      :region,
+      :elb_name,
+      :elb_port,
+      :ssl_certificate_name,
+      :signature_algorithm
+    ) do
       def to_s
         values.join("\t")
       end
@@ -19,6 +27,7 @@ module Cecha
 
       @elasticloadbalancing = Aws::ElasticLoadBalancing::Resource.new aws_configuration
       @iam = Aws::IAM::Resource.new aws_configuration
+      @region = aws_configuration[:region]
     end
 
     def list
@@ -29,7 +38,10 @@ module Cecha
           elb.listener_descriptions.each do |desc|
             cert = ssl_certificate(desc.listener.ssl_certificate_id)
             if cert
-              elbcert = ElbCert.new(elb.load_balancer_name)
+              elbcert = ElbCert.new
+              elbcert.aws_account_number = aws_account_number(desc.listener.ssl_certificate_id)
+              elbcert.region = @region
+              elbcert.elb_name = elb.load_balancer_name
               elbcert.elb_port = desc.listener.load_balancer_port
               elbcert.ssl_certificate_name = cert.name
               elbcert.signature_algorithm = OpenSSL::X509::Certificate.new(cert.certificate_body).signature_algorithm
@@ -48,6 +60,10 @@ module Cecha
       @iam.server_certificates.find do |cert|
         cert.server_certificate_metadata.arn == arn
       end
+    end
+
+    def aws_account_number(arn)
+      arn.split(':')[4]
     end
   end
 end
